0d599e9755bb722948a51748f97770acda9cd614
rkoning <rkoning@9b6b0240-9c15-4950-99ad-f55ef948cf6a>
2015-03-11 17:20:21 +0000

    Added basic support for the Ciena 8700 switch.

---
+++ handlers/network/src/main/java/orca/handlers/network/tasks/Ciena8700/Ciena8700BaseTask.java
@@ -0,0 +1,35 @@
+package orca.handlers.network.tasks.Ciena8700;
+
+import orca.handlers.network.router.Ciena8700Device;
+import orca.handlers.network.router.Cisco6509Device;
+import orca.handlers.network.router.IMappingRouterDevice;
+import orca.handlers.network.tasks.SyncNetworkBaseTask;
+
+
+public abstract class Ciena8700BaseTask extends SyncNetworkBaseTask {
+    
+    public static final String Ciena8700 = "ciena8700";
+    protected IMappingRouterDevice router;
+    protected String adminPassword;
+    protected String defaultPrompt;
+
+    @Override
+    protected void makeDevice() {
+    	super.makeDevice();
+    	
+        if (deviceInstance.equalsIgnoreCase(Ciena8700)) {
+            router = new Ciena8700Device(deviceAddress, user, password, defaultPrompt);
+        } else {
+            throw new RuntimeException("Unsupported router: " + deviceInstance);
+        }        
+        configureDevice(router);
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+
+    public void setDefaultPrompt(String prompt) {
+    	this.defaultPrompt = prompt;
+    }
+}
