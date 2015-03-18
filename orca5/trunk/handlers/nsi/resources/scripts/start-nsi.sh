c9719b7f72d900e68b959b3159b84fe58ed0a397
dromao <dromao@9b6b0240-9c15-4950-99ad-f55ef948cf6a>
2015-03-18 18:43:43 +0000

    Changes on the NSI handler: bug fixes, improvements, and support for multiple stitch ports

--- orca5/trunk/handlers/nsi/resources/scripts/start-nsi.sh
+++ orca5/trunk/handlers/nsi/resources/scripts/start-nsi.sh
@@ -9,7 +9,7 @@
 # If NSI_VERIFY is not set, the certificate will be verified (default = true)
 # If NSI_DEBUG is not set, no additional output will be shown (default = false)
 # If NSI_BW is not set, it will be 100 Mbps (handler.xml)
-# Either NSI_START_TIME and NSI_END_TIME, or NSI_DURATION, must be set
+# Either NSI_START_TIME and NSI_END_TIME, or NSI_DURATION, must be set. The minimum NSI_START_TIME is 10 seconds.
 
 # ORCA uses bps and OpenNSA Mbps
 NSI_BW=$(($NSI_BW/1000000))
@@ -20,12 +20,15 @@ fi
 
 if [ "$NSI_START_TIME" = "\${config.start_time}" ] || [ "$NSI_END_TIME" = "\${config.end_time}" ]; then
 	if [ ${NSI_DURATION} ]; then
-	    # Tweak this value. NTP helps here as the time needs to be in sync with the OpenNSA server.
 	    NSI_START_TIME=10
 		NSI_END_TIME=$((NSI_DURATION+NSI_START_TIME))
 	else
 		echo "start-nsi.sh: NSI_DURATION, NSI_START_TIME and NSI_END_TIME, not set" >&2 
-		echo "start-nsi.sh: Either NSI_DURATION or NSI_START_TIME and NSI_END_TIME must be set" >&2 
+		echo "start-nsi.sh: Either NSI_DURATION or NSI_START_TIME and NSI_END_TIME must be set" >&2
+	fi
+else
+	if [ "$NSI_START_TIME" -lt 10 ]; then
+	  	NSI_START_TIME=10
 	fi
 fi
 
@@ -49,35 +52,24 @@ if [ "$NSI_DEBUG" = "true" ]; then
 	VERBOSE="-v" # verbose output
 fi
 
-# FIXME: Remove and fix appropriately after SC14 demos
-# Hardcoded just for testing
-NSI_START_TIME=10
-NSI_END_TIME=$((NSI_END_TIME+NSI_START_TIME))
-
-# send stdout there
-# FIXME: use proper logging location, use a property for the location
-# Also, this is a security hole *waiting* to be exploited via the trap.
-OUTFILE="/tmp/nsi.tmp"
-trap 'rm -f $OUTFILE' EXIT
-
 for CURR_TAG in $(echo ${NSI_TAGS} | tr "," " "); do
-   
+
 	if [ "${VERBOSE}" ]; then
-    	echo "command: ${NSI_PYTHON} ${NSI_ONSA} reserveprovision -r ${NSI_REQUESTER} -p ${NSI_PROVIDER} -s ${NSI_L2_SRC}#${CURR_TAG} -d ${NSI_L2_DST}#${CURR_TAG} -j user=${NSI_USER} -u ${NSI_SERVICE} -a +${NSI_START_TIME} -e +${NSI_END_TIME} -l ${NSI_HOSTCERT} -k ${NSI_HOSTKEY} -i ${NSI_CERTDIR} -b ${NSI_BW} ${TLS} ${VERIFY} ${VERBOSE} > ${OUTFILE}" >&2 
+	    echo "command: ${NSI_PYTHON} ${NSI_ONSA} reserveprovision -r ${NSI_REQUESTER} -p ${NSI_PROVIDER} -s ${NSI_L2_SRC}#${CURR_TAG} -d ${NSI_L2_DST}#${CURR_TAG} -j user=${NSI_USER} -u ${NSI_SERVICE} -a +${NSI_START_TIME} -e +${NSI_END_TIME} -l ${NSI_HOSTCERT} -k ${NSI_HOSTKEY} -i ${NSI_CERTDIR} -b ${NSI_BW} ${TLS} ${VERIFY} ${VERBOSE}" >&2 
 	fi
 
-	$NSI_PYTHON $NSI_ONSA reserveprovision -r "$NSI_REQUESTER" -p "$NSI_PROVIDER" -s "$NSI_L2_SRC"#"$CURR_TAG" -d "$NSI_L2_DST"#"$CURR_TAG" -j user="$NSI_USER" -u "$NSI_SERVICE" -a +"$NSI_START_TIME" -e +"$NSI_END_TIME" -l "$NSI_HOSTCERT" -k "$NSI_HOSTKEY" -i "$NSI_CERTDIR" -b "$NSI_BW" "$TLS" "$VERIFY" "$VERBOSE" > "$OUTFILE"
+	OUTPUT=$($NSI_PYTHON $NSI_ONSA reserveprovision -r "$NSI_REQUESTER" -p "$NSI_PROVIDER" -s "$NSI_L2_SRC"#"$CURR_TAG" -d "$NSI_L2_DST"#"$CURR_TAG" -j user="$NSI_USER" -u "$NSI_SERVICE" -a +"$NSI_START_TIME" -e +"$NSI_END_TIME" -l "$NSI_HOSTCERT" -k "$NSI_HOSTKEY" -i "$NSI_CERTDIR" -b "$NSI_BW" "$TLS" "$VERIFY" "$VERBOSE")
 
-	CONNECTION_ID=`grep provisioned ${OUTFILE} | cut -d ' ' -f 2`
-	NO_AVAIL=`grep "No type mapping for tag" ${OUTFILE}`
+	CONNECTION_ID=`echo "$OUTPUT" | grep "${NSI_PROVISIONEDS}" | cut -d ' ' -f 2`
+	NO_AVAIL=`echo "$OUTPUT" | grep "${NSI_VLANNAS}"`
 
 	if [ ${CONNECTION_ID} ]; then
 		# This should be the only content of stdout
 		echo $CONNECTION_ID" "$CURR_TAG
 		exit 0
-	elif [ -z "$NO_AVAIL" ]; then
+    elif [ -z "$NO_AVAIL" ]; then
 		echo "Reservation failed for an unknown reason." >&2
-		cat ${OUTFILE} >&2
+		echo "$OUTPUT" >&2
     	exit 1
 	fi
 
