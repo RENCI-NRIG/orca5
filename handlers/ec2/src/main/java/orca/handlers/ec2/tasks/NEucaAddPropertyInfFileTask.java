df1e564f364d023eaa7dceffc8cd4d5b925ad492
paul-ruth <paul.ruth@gmail.com>
2015-07-17 10:03:06 -0400

    abandoned the use of the ini parsing classes becuase we we don't follow their rules

--- handlers/ec2/src/main/java/orca/handlers/ec2/tasks/NEucaAddPropertyInfFileTask.java
+++ handlers/ec2/src/main/java/orca/handlers/ec2/tasks/NEucaAddPropertyInfFileTask.java
@@ -20,26 +20,14 @@ import org.apache.tools.ant.BuildException;
 import org.ini4j.Ini;
 
 
-/*
-public class  NEucaInfFileGenerator { 
-    org.apache.tools.ant.Project project;
-
-    protected org.apache.tools.ant.Project getProject(){
-        return project;
-    }
-
-    abstract public void doIt(PrintWriter out) throws Exception;
-
-    abstract public String getOutputProperty();
-
-    protected String sanitizeBootScript(String script) {
-        if (script != null)
-	    return script.replaceAll("\n[\\s]*\n", "\n").replaceAll("\n", "\n\t");
-        return script;
-    }
-    
-}
-*/
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.EOFException;
+import java.io.StringReader;
+import java.io.IOException;
+import java.io.FileReader;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 
 public class NEucaAddPropertyInfFileTask extends OrcaAntTask{
@@ -52,7 +40,56 @@ public class NEucaAddPropertyInfFileTask extends OrcaAntTask{
     protected String section = null;
     protected String key = null;
     protected String value = null;
+    
+    /*
+    private void addProperty_Ini(){
+        System.out.println("PRUTH: this.section = " + this.section + ", this.key = " + this.key + ", this.value = " + this.value);
 
+        Ini ini = null;
+        if (this.file.equals(this.userdataOld)){
+            System.out.println("PRUTH: reading userdata from file " + this.file);
+            ini = new Ini(new File(this.file));
+        } else {
+            System.out.println("PRUTH: using userdata from userdataOld string");
+            ini = new Ini(new ByteArrayInputStream(this.userdataOld.getBytes()));
+	}
+
+        if(this.section != null && this.key != null && this.value != null){
+            System.out.println("PRUTH: adding this.section = " + this.section + ", this.key = " + this.key + ", this.value = " + this.value);
+            ini.put(this.section,this.key,this.value);
+	} else {
+            System.out.println("PRUTH: skipping add");
+        }
+
+
+        ini.store(new File(file));
+
+    }
+    */
+/*
+    private void addProperty(){
+	System.out.println("PRUTH: this.section = " + this.section + ", this.key = " + this.key + ", this.value = " + this.value);
+
+	if (this.file.equals(this.userdataOld)){
+	    System.out.println("PRUTH: reading userdata from file " + this.file);
+	    ini = new Ini(new File(this.file));
+	} else {
+	    System.out.println("PRUTH: using userdata from userdataOld string");
+	    ini = new Ini(new ByteArrayInputStream(this.userdataOld.getBytes()));
+	}
+	
+	if(this.section != null && this.key != null && this.value != null){
+	    System.out.println("PRUTH: adding this.section = " + this.section + ", this.key = " + this.key + ", this.value = " + this.value);
+	    ini.put(this.section,this.key,this.value);
+	} else {
+	    System.out.println("PRUTH: skipping add");
+	}
+
+
+	ini.store(new File(file));
+
+    }
+ */
 
     public void execute() throws BuildException {
 		
@@ -64,36 +101,85 @@ public class NEucaAddPropertyInfFileTask extends OrcaAntTask{
 	    if (cloudType == null) {
 		throw new Exception("Missing cloudType parameter");
 	    }
-            //PrintWriter out = new PrintWriter(new FileWriter(new File(file)));
+            
+	    
 
             System.out.println("file: " + file + ", cloudType: " +cloudType);
-         
-
+	  
 
 	    System.out.println("PRUTH: this.section = " + this.section + ", this.key = " + this.key + ", this.value = " + this.value);
-	    
-	    Ini ini = null;
+	    BufferedReader userdataSource;
 	    if (this.file.equals(this.userdataOld)){
-		System.out.println("PRUTH: reading userdata from file " + this.file);
-		ini = new Ini(new File(this.file));
+                System.out.println("PRUTH: reading userdata from file " + this.file);
+		userdataSource = new BufferedReader(new FileReader(this.file)); 
 	    } else {
 		System.out.println("PRUTH: using userdata from userdataOld string");
-		ini = new Ini(new ByteArrayInputStream(this.userdataOld.getBytes()));
-	    }
+                userdataSource = new BufferedReader(new StringReader(this.userdataOld));
+            }
 
-	    if(this.section != null && this.key != null && this.value != null){
+  
+	    System.out.println("PRUTH: this.section = " + this.section + ", this.key = " + this.key + ", this.value = " + this.value);
+	    String userdataNew = "";
+	    //if(this.section != null && this.key != null && this.value != null){
 		System.out.println("PRUTH: adding this.section = " + this.section + ", this.key = " + this.key + ", this.value = " + this.value);
-		ini.put(this.section,this.key,this.value);
-	    } else {
-		System.out.println("PRUTH: skipping add");
-	    }
-	    
-	    
-	    ini.store(new File(file));
+		try {
+		    StringBuilder sb = new StringBuilder();
+		    String line = userdataSource.readLine();
+		    
+		    String section = "";
+		    String key = "";
+
+		    while (line != null) {
+			System.out.println("PRUTH: line = " + line);
+
+			//String sectionNext = line.replaceAll("^.*[", "");
+			//sectionNext = line.replaceAll("^.*\[", "");
+			
+			//if (line.matches("~\\[users\\].*")){
+			//    System.out.println("PRUTH: line starts with users = " + line);
+			//    section="users";
+			//}
+			
+			//if (line.matches("^\\[.*\\].*")){
+                        //    System.out.println("PRUTH: line matches section = " + line);
+			//    section="";
+			//}
+
+			Pattern pattern = Pattern.compile("^\\[(.*?)\\]");
+			Matcher matcher = pattern.matcher(line);
+			if (matcher.find()){
+			    section=matcher.group(1);
+			    System.out.println("found section " + section);
+			}
+			
+			
+			key=line.split("=")[0].trim();
+			System.out.println("processing key: " + key);
+			//find key 
+			
+			if (section.equals("users") && key.equals(this.key)){
+			    sb.append(line.trim() +  this.value.trim() + ":\n");
+			} else {
+			    //System.out.println("PRUTH: sectionNext = " + sectionNext);
+			    sb.append(line);
+			}
+			sb.append(System.lineSeparator());
+			line = userdataSource.readLine();
+		    }
+		    userdataNew = sb.toString();
+		} finally {
+		    userdataSource.close();
+		}
+		//}
+	    userdataSource.close();
+
+	    PrintWriter out = new PrintWriter(new FileWriter(new File(file)));
+	    out.print(userdataNew);
+	    out.close();
 
 
-	    //if(outputProperty != null)
-	    //   getProject().setProperty(userdataNew);
+	    if(outputProperty != null)
+		getProject().setProperty(this.outputProperty,userdataNew);
 	    
   
 	} catch (BuildException e) {
