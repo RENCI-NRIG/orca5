987b5f6f0c8197a33be832c143d6524063f7049b
Alan Hinchliff <hincha@renci.org>
2016-11-16 13:43:02 -0500

    #83: changing docker entrypoints as necessary pre-cursor to loading config in volumes

---
+++ docker/orca_sm/docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+prog="orca_sm-14080"
+exec="/opt/orca/bin/orcad"
+    
+# default: run Orca AM+Broker
+if [ "$1" = "$prog" ]; then
+    [ -e /etc/sysconfig/$prog ] && . /etc/sysconfig/$prog
+
+    chown -R geni-orca:nonrenci /etc/orca
+
+    export ORCA_HOME
+    export ORCA_SERVER_PORT
+
+    #exec $exec start
+    exec /opt/orca/bin/wrapper-linux-x86-64 /opt/orca/conf/wrapper.conf wrapper.syslog.ident=orcad wrapper.pidfile="$ORCA_HOME/run/orcad.pid" wrapper.logfile="$ORCA_HOME/logs/orca-stdout.log" wrapper.daemonize=FALSE
+fi
+
+# allow user to run e.g. /bin/bash
+exec "$@"
+
