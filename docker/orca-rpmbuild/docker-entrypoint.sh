b0674da257da1cbbe8b2e9b6c8acbd41ccf4e4e1
Alan Hinchliff <hincha@renci.org>
2016-12-06 13:33:45 -0500

    more robust orca-rpmbuild docker

---
+++ docker/orca-rpmbuild/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Setup Java correctly
+export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:/bin/java::")
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Setup Java to conform to buildrpm.sh script
+$(mkdir -p /usr/java)
+$(ln -s $JAVA_HOME /usr/java/latest)
+
+# if command starts with an option, prepend our default command
+if [ "${1:0:1}" = '-' ]; then
+	set -- "/root/git/orca5/redhat/buildrpm.sh" "$@"
+fi
+
+exec "$@"
+
