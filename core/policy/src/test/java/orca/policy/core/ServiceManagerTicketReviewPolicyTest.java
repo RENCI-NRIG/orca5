115eddf469d85200ba4886fb4176bb7c6779143e
Alan Hinchliff <hincha@renci.org>
2017-01-27 11:20:20 -0500

    additional TicketReview test

--- core/policy/src/test/java/orca/policy/core/ServiceManagerTicketReviewPolicyTest.java
+++ core/policy/src/test/java/orca/policy/core/ServiceManagerTicketReviewPolicyTest.java
@@ -47,17 +47,13 @@ public class ServiceManagerTicketReviewPolicyTest extends ServiceManagerPolicyTe
 
         // one reservation
         IServiceManagerReservation r1 = ServiceManagerReservationFactory.getInstance().create(resources, term, slice);
-
         r1.setRenewable(false);
-
         sm.register(r1);
         sm.demand(r1.getReservationID());
 
         // second reservation
         IServiceManagerReservation r2 = ServiceManagerReservationFactory.getInstance().create(resources, term, slice);
-
         r2.setRenewable(false);
-
         sm.register(r2);
         sm.demand(r2.getReservationID());
 
@@ -106,17 +102,13 @@ public class ServiceManagerTicketReviewPolicyTest extends ServiceManagerPolicyTe
 
         // one reservation
         IServiceManagerReservation r1 = ServiceManagerReservationFactory.getInstance().create(resources, term, slice);
-
         r1.setRenewable(false);
-
         sm.register(r1);
         sm.demand(r1.getReservationID());
 
         // second reservation
         IServiceManagerReservation r2 = ServiceManagerReservationFactory.getInstance().create(resources, term, slice);
-
         r2.setRenewable(false);
-
         sm.register(r2);
         //sm.demand(r2.getReservationID()); // don't ticket this until later
         boolean r2demanded = false;
@@ -148,4 +140,84 @@ public class ServiceManagerTicketReviewPolicyTest extends ServiceManagerPolicyTe
         }
 
     }
+
+    /**
+     * Test where three reservations are requested.
+     * One of the tickets is failed.
+     * One of the reservations is not demanded, and thus remains Nascent.
+     * The failed and ticketed reservations will not be Closed until the final reservation is no longer Nascent.
+     *
+     * @throws Exception
+     */
+    public void testFailAndNascent() throws Exception {
+        IServiceManager sm = (IServiceManager) getSM();
+
+        ActorClock clock = sm.getActorClock();
+        Term.clock = clock;
+
+        ResourceSet resources = new ResourceSet(1, new ResourceType(1));
+
+        // slice name must start with 'fail' for test to run correctly.
+        // see ServiceManagerTestWrapper.bid()
+        ISlice slice = SliceFactory.getInstance().create("fail_nascent");
+        sm.registerSlice(slice);
+
+        long start = 10;
+        long end = 15;
+
+        Term term = new Term(clock.cycleStartDate(start), clock.cycleEndDate(end));
+
+        // one reservation
+        IServiceManagerReservation r1 = ServiceManagerReservationFactory.getInstance().create(resources, term, slice);
+        r1.setRenewable(false);
+        sm.register(r1);
+        sm.demand(r1.getReservationID());
+
+        // second reservation
+        IServiceManagerReservation r2 = ServiceManagerReservationFactory.getInstance().create(resources, term, slice);
+        r2.setRenewable(false);
+        sm.register(r2);
+        //sm.demand(r2.getReservationID()); // don't ticket this until later
+        boolean r2demanded = false;
+
+        // third reservation
+        IServiceManagerReservation r3 = ServiceManagerReservationFactory.getInstance().create(resources, term, slice);
+        r3.setRenewable(false);
+        sm.register(r3);
+        sm.demand(r3.getReservationID());
+
+
+        for (int i = 1; i <= end+2; i++) {
+            sm.externalTick((long) i);
+            //System.out.println("i: " + i + " r.getState() = " + r.getState());
+            while (sm.getCurrentCycle() != i){
+                sleep(1);
+            }
+
+            // give plenty of time to make sure the Failed ticket is not closed until Nascent is resolved
+            if ( i>2 && !r2demanded ){
+                assertTrue(r1.getState() == ReservationStates.Failed);
+                assertTrue(r2.getState() == ReservationStates.Nascent);
+                assertTrue(r3.getState() == ReservationStates.Ticketed);
+
+                if (i>6) {
+                    sm.demand(r2.getReservationID());
+                    r2demanded = true;
+                }
+            }
+
+            if ((i >= start) && (i < (end - 1))) {
+                assertTrue(r1.getState() == ReservationStates.Closed);
+                assertTrue(r2.getState() == ReservationStates.Closed);
+                assertTrue(r3.getState() == ReservationStates.Closed);
+            }
+
+            if (i > end) {
+                assertTrue(r1.getState() == ReservationStates.Closed);
+                assertTrue(r2.getState() == ReservationStates.Closed);
+            }
+        }
+
+    }
+
 }
