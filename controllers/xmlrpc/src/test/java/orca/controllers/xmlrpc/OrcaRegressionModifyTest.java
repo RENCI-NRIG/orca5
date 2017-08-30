047697c9a6cfc141490ba440e68418c8dba5a9b5
Alan Hinchliff <hincha@renci.org>
2017-08-30 16:52:55 -0400

    adding test for #137

--- controllers/xmlrpc/src/test/java/orca/controllers/xmlrpc/OrcaRegressionModifyTest.java
+++ controllers/xmlrpc/src/test/java/orca/controllers/xmlrpc/OrcaRegressionModifyTest.java
@@ -3,7 +3,11 @@ package orca.controllers.xmlrpc;
 import orca.manage.beans.TicketReservationMng;
 import orca.ndl.NdlCommons;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import static orca.controllers.xmlrpc.OrcaXmlrpcAssertions.assertBootscriptVelocityTemplating;
@@ -11,50 +15,76 @@ import static orca.controllers.xmlrpc.OrcaXmlrpcAssertions.assertManifestWillPro
 import static orca.controllers.xmlrpc.OrcaXmlrpcAssertions.assertReservationsHaveNetworkInterface;
 import static orca.controllers.xmlrpc.OrcaXmlrpcHandlerTest.EXPECTED_RESERVATION_COUNT_FOR_MODIFY;
 
+@RunWith(Parameterized.class)
 public class OrcaRegressionModifyTest {
 
-    /**
-     * Bootscripts need Velocity templating on Modify
-     *
-     * @throws Exception
-     */
-    @Test
-    public void testVelocityOnModify() throws Exception {
-        // modify request
-        String modReq = NdlCommons.readFile("src/test/resources/112_velocityModifyRequest.rdf");
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                //Bootscripts need Velocity templating on Modify
+                {"src/test/resources/112_velocityRequest.rdf",
+                        "src/test/resources/112_velocityModifyRequest.rdf",
+                        5},
+                //NodeGroup modify
+                {"src/test/resources/122_request.rdf",
+                        "src/test/resources/137_modify_request.rdf",
+                        5},
+                //NodeGroup modify
+                {"src/test/resources/137_one_nodegroups_of_size_one_noip_request.rdf",
+                        "src/test/resources/137_one_nodegroups_of_size_one_noip_modify_request.rdf",
+                        3}
+        });
+    }
 
-        XmlrpcControllerSlice slice = OrcaXmlrpcHandlerTest.doTestModifySlice(
-                "modifySlice_testVelocity",
-                "src/test/resources/112_velocityRequest.rdf",
-                modReq, EXPECTED_RESERVATION_COUNT_FOR_MODIFY);
+    // First Parameter -- file name with Request
+    private String requestFilename;
 
-        List<TicketReservationMng> computedReservations = slice.getComputedReservations();
+    // Second Parameter -- whether test should pass
+    private String modifyFilename;
 
-        //additional checks
-        assertBootscriptVelocityTemplating(computedReservations);
+    // Third Parameter -- number of Devices / Network Elements requested
+    private int numDevicesInRequest;
+
+    // JUnit automatically passes in Parameters to constructor
+    public OrcaRegressionModifyTest(String requestFilename, String modifyFilename, int numDevicesInRequest){
+        this.requestFilename = requestFilename;
+        this.modifyFilename = modifyFilename;
+        this.numDevicesInRequest = numDevicesInRequest;
     }
 
     /**
-     * NodeGroup modify was failing on Increase request, when Auto-IP is not used.
+     * run the regression test suite
      *
      * @throws Exception
      */
     @Test
     public void testNodeGroupModifyIncrease() throws Exception {
+        String testName = requestFilename.substring(requestFilename.lastIndexOf('/') + 1, requestFilename.lastIndexOf('.'));
+        testName += "_" + modifyFilename.substring(modifyFilename.lastIndexOf('/') + 1, modifyFilename.lastIndexOf('.'));
+
+        System.out.println("Starting Orca Regression Modify Test " + testName);
+
         // modify request
-        String modReq = NdlCommons.readFile("src/test/resources/137_modify_request.rdf");
+        String modReq = NdlCommons.readFile(modifyFilename);
 
         XmlrpcControllerSlice slice = OrcaXmlrpcHandlerTest.doTestModifySlice(
-                "modifySlice_testNodeGroup",
-                "src/test/resources/122_request.rdf",
-                modReq, EXPECTED_RESERVATION_COUNT_FOR_MODIFY);
+                "modifySlice_test" + testName,
+                requestFilename,
+                modReq, numDevicesInRequest);
 
         List<TicketReservationMng> computedReservations = slice.getComputedReservations();
 
         assertManifestWillProcess(slice);
 
-        // Nodes added in NodeGroup Increase need to have a Network interface
-        assertReservationsHaveNetworkInterface(computedReservations);
+        //additional checks
+        if (requestFilename.contains("112_")) {
+            System.out.println("Checking Velocity Templating");
+            assertBootscriptVelocityTemplating(computedReservations);
+        } else if (requestFilename.contains("122_") || (requestFilename.contains("137_"))) {
+            System.out.println("Checking for Network Interfaces");
+            // Nodes added in NodeGroup Increase need to have a Network interface
+            assertReservationsHaveNetworkInterface(computedReservations);
+        }
 
     }
 
