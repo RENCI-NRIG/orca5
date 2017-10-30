dfe2a5eac1742aa37302f6ab0b578738b2290ea9
Alan Hinchliff <hincha@renci.org>
2017-10-30 16:28:48 -0400

    avoiding daylight savings time discrepancies

--- ndl/src/test/java/orca/ndl/elements/OrcaReservationTermTest.java
+++ ndl/src/test/java/orca/ndl/elements/OrcaReservationTermTest.java
@@ -10,44 +10,42 @@ import static org.junit.Assert.*;
 
 public class OrcaReservationTermTest {
 
-    // copied from ReservationConverter
-    private static final long TWO_WEEKS = TimeUnit.DAYS.toMillis(14);
-    private static final long DEFAULT_MAX_DURATION = TWO_WEEKS;
+    public static final int NUM_DAYS = 1;
 
     @Test
     public void getDurationInMinutes() throws Exception {
         OrcaReservationTerm term = new OrcaReservationTerm();
         term.setStart(new Date());
-        term.setDuration(Math.toIntExact(TimeUnit.MILLISECONDS.toDays(DEFAULT_MAX_DURATION)), 0, 0, 0);
+        term.setDuration(NUM_DAYS, 0, 0, 0);
 
         final int durationInMinutes = term.getDurationInMinutes();
 
-        assertEquals("Duration in Minutes did not match expected", TimeUnit.MILLISECONDS.toMinutes(DEFAULT_MAX_DURATION), durationInMinutes);
+        assertEquals("Duration in Minutes did not match expected", TimeUnit.DAYS.toMinutes(NUM_DAYS), durationInMinutes);
     }
 
     @Test
     public void getDurationInSeconds() throws Exception {
         OrcaReservationTerm term = new OrcaReservationTerm();
         term.setStart(new Date());
-        term.setDuration(Math.toIntExact(TimeUnit.MILLISECONDS.toDays(DEFAULT_MAX_DURATION)), 0, 0, 0);
+        term.setDuration(NUM_DAYS, 0, 0, 0);
 
         final int durationInSeconds = term.getDurationInSeconds();
 
-        assertEquals("Duration in Seconds did not match expected", TimeUnit.MILLISECONDS.toSeconds(DEFAULT_MAX_DURATION), durationInSeconds);
+        assertEquals("Duration in Seconds did not match expected", TimeUnit.DAYS.toSeconds(NUM_DAYS), durationInSeconds);
     }
 
     @Test
     public void setDurationInDays() throws Exception {
         OrcaReservationTerm term = new OrcaReservationTerm();
         term.setStart(new Date());
-        term.setDuration(Math.toIntExact(TimeUnit.MILLISECONDS.toDays(DEFAULT_MAX_DURATION)), 0, 0, 0);
+        term.setDuration(NUM_DAYS, 0, 0, 0);
 
         final Date termEnd = term.end;
 
         Calendar termStart = Calendar.getInstance();
         termStart.setTime(term.start);
 
-        termStart.add(Calendar.MILLISECOND, Math.toIntExact(DEFAULT_MAX_DURATION));
+        termStart.add(Calendar.DAY_OF_YEAR, NUM_DAYS);
 
         assertEquals("Term end did not match expected value.", termStart.getTime(), termEnd);
     }
@@ -56,14 +54,14 @@ public class OrcaReservationTermTest {
     public void setDurationInHours() throws Exception {
         OrcaReservationTerm term = new OrcaReservationTerm();
         term.setStart(new Date());
-        term.setDuration(0, Math.toIntExact(TimeUnit.MILLISECONDS.toHours(DEFAULT_MAX_DURATION)), 0, 0);
+        term.setDuration(0, Math.toIntExact(TimeUnit.DAYS.toHours(NUM_DAYS)), 0, 0);
 
         final Date termEnd = term.end;
 
         Calendar termStart = Calendar.getInstance();
         termStart.setTime(term.start);
 
-        termStart.add(Calendar.MILLISECOND, Math.toIntExact(DEFAULT_MAX_DURATION));
+        termStart.add(Calendar.DAY_OF_YEAR, NUM_DAYS);
 
         assertEquals("Term end did not match expected value.", termStart.getTime(), termEnd);
     }
@@ -72,14 +70,14 @@ public class OrcaReservationTermTest {
     public void setDurationInMinutes() throws Exception {
         OrcaReservationTerm term = new OrcaReservationTerm();
         term.setStart(new Date());
-        term.setDuration(0, 0, Math.toIntExact(TimeUnit.MILLISECONDS.toMinutes(DEFAULT_MAX_DURATION)), 0);
+        term.setDuration(0, 0, Math.toIntExact(TimeUnit.DAYS.toMinutes(NUM_DAYS)), 0);
 
         final Date termEnd = term.end;
 
         Calendar termStart = Calendar.getInstance();
         termStart.setTime(term.start);
 
-        termStart.add(Calendar.MILLISECOND, Math.toIntExact(DEFAULT_MAX_DURATION));
+        termStart.add(Calendar.DAY_OF_YEAR, NUM_DAYS);
 
         assertEquals("Term end did not match expected value.", termStart.getTime(), termEnd);
     }
@@ -88,14 +86,14 @@ public class OrcaReservationTermTest {
     public void setDurationInSeconds() throws Exception {
         OrcaReservationTerm term = new OrcaReservationTerm();
         term.setStart(new Date());
-        term.setDuration(0, 0, 0, Math.toIntExact(TimeUnit.MILLISECONDS.toSeconds(DEFAULT_MAX_DURATION)));
+        term.setDuration(0, 0, 0, Math.toIntExact(TimeUnit.DAYS.toSeconds(NUM_DAYS)));
 
         final Date termEnd = term.end;
 
         Calendar termStart = Calendar.getInstance();
         termStart.setTime(term.start);
 
-        termStart.add(Calendar.MILLISECOND, Math.toIntExact(DEFAULT_MAX_DURATION));
+        termStart.add(Calendar.DAY_OF_YEAR, NUM_DAYS);
 
         assertEquals("Term end did not match expected value.", termStart.getTime(), termEnd);
     }
