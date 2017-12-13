de90663db27e10a158a938da8a7eb5fa8ac5df67
Alan Hinchliff <hincha@renci.org>
2017-12-13 11:44:16 -0500

    deleting old unused files

--- controllers/_ben/resources/web/interdomain/js/yuidomcollapse-css.js
+++
@@ -1,9 +0,0 @@
-if(YAHOO && YAHOO.otorg && YAHOO.otorg.DomCollapse){
-	YAHOO.otorg.DomCollapse.css = {
-		triggerClass:'trigger',
-		hideClass:'hide',
-		parentClass:'parent',
-		openClass:'open'
-	}
-}
-	
