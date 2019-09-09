658c51de60c219902b6a72e9482c0206e546a07e
mcevik0 <mcevik@renci.org>
2019-09-09 03:34:07 -0400

    Modify scripts for running floodlight in docker containers

--- handlers/providers/resources/scripts/stop_floodlight.sh
+++ handlers/providers/resources/scripts/stop_floodlight.sh
@@ -9,12 +9,26 @@ if [ -z "$FVVLAN" ]; then
 	exit 1
 fi
 
-if [[ -z "$USEPS" || ! -n "$USEPS" ]]; then
-	NOXPID=`pgrep -f "net.orca.vlan=vlan-$FVVLAN-"`
+
+STATUS_RUNNING="--filter status=running"
+STATUS_CREATED="--filter status=created"
+STATUS_RESTARTING="--filter status=restarting"
+STATUS_REMOVING="--filter status=removing"
+STATUS_PAUSED="--filter status=paused"
+STATUS_EXITED="--filter status=exited"
+STATUS_DEAD="--filter status=dead"
+
+DOCKER_CONTAINER_NAME="floodlight-${FVVLAN}"
+
+RUNNING_CONT=$(docker ps ${STATUS_RUNNING} --format "{{.Names}}" --filter "name=${DOCKER_CONTAINER_NAME}")
+
+if [ "${RUNNING_CONT}" == ${DOCKER_CONTAINER_NAME} ]; then
+    docker stop  ${RUNNING_CONT} 
+    docker rm ${RUNNING_CONT}
 else
-	NOXPID=`ps axw | grep nox | grep net.orca.vlan=vlan-$FVVLAN- | awk '{ print $1}'`
+    CONT=$(docker ps --format "{{.Names}}" --filter "name=${DOCKER_CONTAINER_NAME}" ${STATUS_EXITED} ${STATUS_CREATED} ${STATUS_RESTARTING} ${STATUS_REMOVING} ${STATUS_DEAD} ${STATUS_PAUSED})
+    if [ "${CONT}" == ${DOCKER_CONTAINER_NAME} ]; then
+        docker rm ${CONT}
+    fi
 fi
-	
-if [ ! -z "$NOXPID" ]; then
-	kill -TERM $NOXPID
-fi
+ 
